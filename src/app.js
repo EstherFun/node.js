fa1530c9f3d998de30ba2767099ade5bfcf423b4
Esther Funmilayo <ilovemepurple1112@gmail.com>
2023-04-08 13:49:45 +0100

    commtting

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome!');
-});
+//app.get('/', (req, res) => {
+//    res.send('Welcome!');
+//});
 
 app.listen(PORT, () => {
     console.log('App listening on port ' + PORT);
