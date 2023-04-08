64fe0b846c8dda3855461c33f8a04346869efff4
Esther Funmilayo <ilovemepurple1112@gmail.com>
2023-04-08 13:47:01 +0100

    commtting

--- src/app.js
+++ src/app.js
@@ -1,3 +1,46 @@
-const { v4: uuidv4}= require('uuid');
+const express = require('express');
+//const mongoose = require('mongoose');
+const app = express();
+//mongoose.set('strictQuery', false);
 
-console.log(uuidv4())
+//app.use(express.json());
+//app.use(express.urlencoded({extended:true}));
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+});
+
+/*const customers = [
+    {
+        "name": "Caleb",
+        "industry": "music"
+    }
+];
+
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
+});
+app.post('/api/customers', (req, res) => {
+    console.log(req, res);
+    res.send(req.body);
+});
+app.post('/', (req, res) => {
+    res.send('This is a post request!');
+});
+
+const start = async() => {
+    await mongoose.connect('mongodb+srv://EstherF:ecIPPDfSqcw8AKoF@cluster0.om2uzah.mongodb.net/?retryWrites=true&w=majority');
+
+    app.listen(PORT, () => {
+        console.log('App listening on port ' + PORT);
+    });
+};
+
+start();*/
